fa59386235a5f0962852923954eaf3c8d3cf43fe
alixth54 <thesealix@yahoo.fr>
2024-02-19 16:21:25 +0100

    logger
    ajout de logger sur maincontroller recuperation message d'erreur dans var/log/dev.log

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,27 +9,50 @@ use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Tache;
 use App\Entity\Category;
 use Symfony\Component\Translation\LocaleSwitcher;
+use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\ORM\MyCustomException;
 
 class MainController extends AbstractController
 {
     
 
     #[Route('/{_locale}', name: 'app_main')]
-    public function accueil(): Response
+    public function accueil(LoggerInterface $logger): Response
     {
-        
-        
+       try{
+        $logger->info('connection à la bdd');
         return $this->render('main/index.html.twig');
+       } 
+       catch(EntityNotFoundException $e){
+        $logger->error($e);
+       }
+         
     }
+
     // pour afficher element tache dans la table
     #[Route('/{_locale}/tasks', name: 'app_task')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, LoggerInterface $logger): Response
     {
-        $tasks = $entityManager->getRepository(Tache::class)->findBy(['user'=>$this->getUser()]);
+    
+        try{
+        $logger->debug('User {userId} has logged in', [
+            'userId' => $this->getUser()->getId()
+        ]);
+            $tasks = $entityManager->getRepository(Tache::class)->findBy(['user'=>$this->getUser()]);
         
         return $this->render('tasks/task.html.twig', [
             'tasks' => $tasks,
         ]);
+        }catch(\Exception $e){
+            $logger->error('le message d\'erreur est : {e}', [
+                'e' => $e
+            ]);
+        }
+         
+            
+           
+        
     }
 
     #[Route('/{_locale}/category', name: 'app_category')]
